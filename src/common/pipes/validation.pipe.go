93cf1845d314e0d76eaa04a7cef4bc0397592616
JsCodeDevlopment <jonatasilva118@gmail.com>
2026-03-17 22:54:05 -0300

    fix: enhance error handling in validation by integrating i18n for localized error messages

--- src/common/pipes/validation.pipe.go
+++ src/common/pipes/validation.pipe.go
@@ -1,6 +1,7 @@
 package pipes
 
 import (
+	"gost/src/common/i18n"
 	"gost/src/common/utils"
 	"net/http"
 
@@ -10,7 +11,7 @@ import (
 func ValidateBody[T any](c *gin.Context) (*T, error) {
 	var dto T
 	if err := c.ShouldBindJSON(&dto); err != nil {
-		utils.FormattedErrorGenerator(c, http.StatusBadRequest, "Bad Request", err.Error())
+		utils.FormattedErrorGenerator(c, http.StatusBadRequest, "Bad Request", i18n.FormatValidationError(c, err))
 		return nil, err
 	}
 	return &dto, nil
